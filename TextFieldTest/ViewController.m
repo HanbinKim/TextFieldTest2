455dc3583db91d77aa7d0e1ab091dddb280f86f9
HanbinKim <hottuc@naver.com>
2014-01-03 18:08:28 +0900

    107P 실습 완료하였습니다.
    텍스트필드와 레이블 코드로만 3개 작성하여
    첫번재 텍스트필드는 리턴시 값 출력 두번째 텍스트필드는 아예 편집불가 세번째 텍스트 필드는 z값 입력불가능으로 만들었습니다.

--- TextFieldTest/ViewController.m
+++ TextFieldTest/ViewController.m
@@ -18,6 +18,61 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    UILabel *label1 = [[UILabel alloc] initWithFrame:CGRectMake(40, 25, 0, 0)];
+    label1.text = @"Return";
+    label1.textColor = [UIColor whiteColor];
+    label1.backgroundColor = [UIColor clearColor];
+    [label1 sizeToFit];
+    [self.view addSubview:label1];
+    UILabel *label2 = [[UILabel alloc] initWithFrame:CGRectMake(40, 75, 0, 0)];
+    label2.text = @"편집 불가";
+    label2.textColor = [UIColor whiteColor];
+    label2.backgroundColor = [UIColor clearColor];
+    [label2 sizeToFit];
+    [self.view addSubview:label2];
+    UILabel *label3 = [[UILabel alloc] initWithFrame:CGRectMake(40, 125, 0, 0)];
+    label3.text = @"Z 입력금지";
+    label3.textColor = [UIColor whiteColor];
+    label3.backgroundColor = [UIColor clearColor];
+    [label3 sizeToFit];
+    [self.view addSubview:label3];
+    
+    UITextField *textField = [[UITextField alloc] initWithFrame:CGRectMake(130, 20, 171, 31)];
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    textField.delegate = self;
+    textField.tag = 1;
+    UITextField *textField2 = [[UITextField alloc] initWithFrame:CGRectMake(130, 70, 171, 31)];
+    textField2.borderStyle = UITextBorderStyleRoundedRect;
+    textField2.delegate = self;
+    textField2.tag = 2;
+    UITextField *textField3 = [[UITextField alloc] initWithFrame:CGRectMake(130, 120, 171, 31)];
+    textField3.borderStyle = UITextBorderStyleRoundedRect;
+    textField3.delegate = self;
+    textField3.tag = 3;
+    
+    [self.view addSubview:textField];
+    [self.view addSubview:textField2];
+    [self.view addSubview:textField3];
+    
+}
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField{
+    if(2==textField.tag)
+        return NO;
+    return YES;
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    NSLog(@"INPUT : %@",textField.text);
+    [textField resignFirstResponder];
+    return YES;
+}
+
+-(BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+    if(3 == textField.tag && NSOrderedSame == [string compare:@"z" options:NSCaseInsensitiveSearch])
+        return NO;
+    return YES;
 }
 
 - (void)didReceiveMemoryWarning
